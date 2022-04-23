66489fa3d8dbdd6dd2c1d6f3246377c742a607dd
Arynn Boniface <arynn.boniface@gmail.com>
2022-04-23 18:51:50 -0400

    all of the image assets

--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import tw, { styled } from "twin.macro";
 
 // ========== TYPES ==========
+interface InputProps {
+	label: string;
+	name: string;
+	placeholder: string;
+	type: `text` | `email` | `number` | `tel`;
+	register?: any;
+	disabled?: boolean;
+	required?: boolean;
+}
 // ========== STYLES ==========
 
 // ========== COMPONENTS ==========
-const Input = () => {};
+const Input = ({ label, name, placeholder, register, disabled, required }: InputProps) => {};
 
 export default Input;
