cc431041471a27cf9f346848fee145dd6767d201
Arynn Boniface <arynn.boniface@gmail.com>
2022-04-23 14:23:21 -0400

    font types and formats

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from "next/app";
 import { GlobalStyles } from "twin.macro";
-import "../styles/globals.css";
+import "../assets/styles/globals.css";
+import "../assets/styles/fonts.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<GlobalStyles />
-			<div>
+			<div tw="antialiased">
 				<Component {...pageProps} />
 			</div>
 		</>
