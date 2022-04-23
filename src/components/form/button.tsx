531aa3b576ded7f92ec94118cff544309a567ad7
Arynn Boniface <arynn.boniface@gmail.com>
2022-04-23 15:28:55 -0400

    folder structure

---
+++ src/components/form/button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import tw, { styled } from "twin.macro";
+
+// ========== TYPES ==========
+// ========== STYLES ==========
+
+// ========== COMPONENTS ==========
+const Button = () => {};
+
+export default Button;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,39 @@
 module.exports = {
-	colors: {
-		white: `#FFFFFF`,
-		grey: {
-			100: `#FAFAFA`,
-			200: `#F1F1F1`,
-			300: `#101010`,
+	theme: {
+		colors: {
+			white: `#FFFFFF`,
+			grey: {
+				100: `#FAFAFA`,
+				200: `#F1F1F1`,
+				300: `#101010`,
+			},
+			peach: `#FBAF85`,
+			orange: `#D87D4A`,
+			red: `#CD2C2C`,
+			black: `#000000`,
+			current: `currentColor`,
+			transparent: `transparent`,
 		},
-		peach: `#FBAF85`,
-		orange: `#D87D4A`,
-		red: `#CD2C2C`,
-		black: `#000000`,
-	},
-	fontSize: {
-		"xs": `13px`,
-		"sm": `14px`,
-		"base": `15px`,
-		"lg": `18px`,
-		"xl": `24px`,
-		"2xl": `28px`,
-		"3xl": `32px`,
-		"4xl": `40px`,
-		"5xl": `56px`,
-	},
-	fontWeight: {
-		light: 300,
-		regular: 400,
-		medium: 500,
-		semi: 600,
-		bold: 700,
-	},
-	fontFamily: {
-		manrope: [`Manrope`, `sans-serif`],
+		fontSize: {
+			"xs": `13px`,
+			"sm": `14px`,
+			"base": `15px`,
+			"lg": `18px`,
+			"xl": `24px`,
+			"2xl": `28px`,
+			"3xl": `32px`,
+			"4xl": `40px`,
+			"5xl": `56px`,
+		},
+		fontWeight: {
+			light: 300,
+			regular: 400,
+			medium: 500,
+			semi: 600,
+			bold: 700,
+		},
+		fontFamily: {
+			manrope: [`Manrope`, `sans-serif`],
+		},
 	},
 };
