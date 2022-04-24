892cef9a35d1736a6c5664ddf0a64e42d0fc9b57
Arynn Boniface <arynn.boniface@gmail.com>
2022-04-24 03:47:12 -0400

    mobile menu tablet

--- src/components/layout/mobile.tsx
+++ src/components/layout/mobile.tsx
@@ -6,14 +6,21 @@ import Button from "../form/button";
 const Container = styled.nav`
 	${tw`bg-white rounded-b-lg`};
 	${tw`px-6 py-8`};
+	${tw`px-10 py-14`};
+	${tw`md:(flex items-center justify-between)`};
 `;
 
 const Item = styled.div`
 	${tw`width[20.3125rem] height[10.3125rem]`};
+	${tw`md:(width[13.9375rem] height[10.3125rem])`};
 	${tw`flex flex-col items-center`};
 	${tw`mb-4 mx-auto rounded-lg`};
 	${tw`text-black bg-grey-200`};
 	${tw`line-height[20.49px] letter-spacing[1.07px] font-bold`};
+
+	.image {
+		${tw`height[6.5rem]`};
+	}
 `;
 
 // ========== COMPONENTS ==========
