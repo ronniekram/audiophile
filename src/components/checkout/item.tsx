03801acbfe676b34c2d3aa4628ec801dcaafe8a3
Arynn Boniface <arynn.boniface@gmail.com>
2022-04-24 00:37:26 -0400

    cart item

--- src/components/checkout/item.tsx
+++ src/components/checkout/item.tsx
@@ -1,18 +1,43 @@
+import Image from "next/image";
 import tw, { styled } from "twin.macro";
 
 // ========== TYPES ==========
-interface ItemProps {
-	image: any;
+export type Item = {
+	image: string;
 	name: string;
 	price: number;
 	quantity: number;
-}
+};
 
-// image: 4rem
-// backdrop-brightness-50, rounded-lg
 // ========== STYLES ==========
 const Container = styled.div`
 	${tw`flex items-center w-full`};
 	${tw`w-full rounded-lg`};
+
+	.image {
+		${tw`w-16 h-16`};
+		${tw`mr-4`};
+		${tw`rounded-lg`};
+		${tw`backdrop-brightness-50`};
+	}
 `;
+
 // ========== COMPONENTS ==========
+const CartItem = ({ image, name, price, quantity }: Item) => {
+	return (
+		<Container>
+			<div>
+				<Image src={image} className="image" />
+			</div>
+			<div tw="line-height[25px] text-black text-base font-bold">
+				<div tw="flex items-center justify-between">
+					<p tw="uppercase">{name}</p>
+					<p tw="text-opacity-50">x{quantity}</p>
+				</div>
+				<p tw="text-opacity-50 text-sm">{`$${price}`}</p>
+			</div>
+		</Container>
+	);
+};
+
+export default CartItem;
